c165a5bc00c12bbd9effa54e93f760241344fe25
camilo1962 <108455047+camilo1962@users.noreply.github.com>
2024-03-19 08:55:16 +0100

    Initial commit

---
+++ Assets/Scripts/Menus.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menus : MonoBehaviour
+{
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+    public void Cambiar(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+    }
+}
